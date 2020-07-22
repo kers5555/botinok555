ce0e8382700ef7640c8d31dea557e8cccbdc5795
kers5555 <68428506+kers5555@users.noreply.github.com>
2020-07-22 13:29:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -272,5 +272,6 @@ async def help(ctx):
         emb.add_field( name = '{}'.format('clear'), value = 'Команда клира Доступ А.С.М' )
 
         await ctx.send(embed = emb)
+
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
